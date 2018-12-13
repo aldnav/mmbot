f5e0e78ceacae3a0d3fdbff5c5dce6d6a40d784c
Aldrin Navarro <aldrinnavarro16@gmail.com>
2018-12-13 14:38:11 +0800

    Let's go

---
+++ server.py
@@ -0,0 +1,30 @@
+import requests as reqs
+import yaml
+import sys
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+settings = None
+try:
+    with open('./conf.yaml', 'r') as f:
+        settings = yaml.load(f)
+except FileNotFoundError:
+    sys.exit('No settings file found. See sample "conf.sample.yaml"')
+
+endpoint = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+
+
+@app.route('/l10n', methods=['POST'])
+def localization():
+    lang = 'zh'
+    text_to_translate = request.values.get('text')
+    r = reqs.post(endpoint, params=dict(
+        key=settings.get('yandex_key'),
+        text=text_to_translate,
+        lang=lang
+    ))
+    response_text = "{}\n{}".format(text_to_translate, r.json()['text'][0])
+    return jsonify({
+        'response_type': 'in_channel',
+        'text': response_text
+    })
